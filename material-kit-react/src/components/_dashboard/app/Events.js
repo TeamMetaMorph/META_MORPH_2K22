875ec5da9c72c91093bd5e8d2d43b695a5e3fd76
sarthak rajput <sarthakr748@gmail.com>
2022-02-19 13:28:33 +0530

    added

---
+++ material-kit-react/src/components/_dashboard/app/Events.js
@@ -0,0 +1,46 @@
+import { Icon } from '@iconify/react';
+import appleFilled from '@iconify/icons-ant-design/apple-filled';
+// material
+import { alpha, styled } from '@mui/material/styles';
+import { Card, Typography } from '@mui/material';
+// utils
+import { fShortenNumber } from '../../../utils/formatNumber';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Card)(({ theme }) => ({
+  boxShadow: 'none',
+  textAlign: 'center',
+  padding: theme.spacing(5, 0),
+  height: theme.spacing(27),
+  color: theme.palette.info.darker,
+  backgroundColor: theme.palette.info.lighter
+}));
+
+const IconWrapperStyle = styled('div')(({ theme }) => ({
+  margin: 'auto',
+  display: 'flex',
+  borderRadius: '50%',
+  alignItems: 'center',
+  width: theme.spacing(8),
+  height: theme.spacing(8),
+  justifyContent: 'center',
+  marginBottom: theme.spacing(3),
+  color: theme.palette.info.dark,
+  backgroundImage: `linear-gradient(135deg, ${alpha(theme.palette.info.dark, 0)} 0%, ${alpha(
+    theme.palette.info.dark,
+    0.24
+  )} 100%)`
+}));
+
+// ----------------------------------------------------------------------
+
+const TOTAL = 1352831;
+
+export default function Events() {
+  return (
+    <RootStyle className="re">
+      <h2 style={{ marginTop: 48 }}>Events</h2>
+    </RootStyle>
+  );
+}
